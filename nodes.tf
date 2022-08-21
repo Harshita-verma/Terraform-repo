2aaabe1de1152be67c6a0df40cd5036d7a5179cd
Harshita-verma <harshitaverma997@gmail.com>
2022-08-21 15:01:24 +0530

    add

--- nodes.tf
+++ nodes.tf
@@ -48,11 +48,15 @@ resource "aws_eks_node_group" "node" {
   capacity_type  = "ON_DEMAND"
   instance_types = ["t3.small"]
 
+
+  
+
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 2
+    max_size     = 3
+    min_size     = 2
   }
+
 
 
 
