97fd23361ac88a334e4740ba9f0a741a4e632e77
Harshita-verma <66748440+Harshita-verma@users.noreply.github.com>
2022-09-06 13:37:35 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 variable "access_key" {
   type    = string
-  default = "AKIAQOELGETRWWIMKCNQ"
+  default = ""
 }
 
 variable "secret_key" {
   type    = string
-  default = "tJCBQH7Q9s1xUXt1eLu7m9XU9iVEJjkljDs0hv5u"
+  default = ""
 }
